8da6e05b6e53778c91928103960e7e7a4069d607
Naveen S.R <naveen@uitoux.in>
2024-01-12 15:43:02 +0530

    Update Configure

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/react-sample-web/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    // other build configurations...
+  },
 })
