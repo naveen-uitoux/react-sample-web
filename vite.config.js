689132fa3e53f1e5069008812914e2b6b7532b9e
Naveen S.R <naveen@uitoux.in>
2024-01-12 15:58:29 +0530

    Changfes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/react-sample-web/',
   plugins: [react()],
   build: {
     outDir: 'dist',
